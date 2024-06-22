dd1cbbb0e3712810abe45bf92149c6b15ffc72c4
鈴木安美 <ami.comsize@gmail.com>
2024-06-23 01:36:29 +0900

    型定義修正

--- pages/api/slack.ts
+++ pages/api/slack.ts
@@ -1,13 +1,42 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { WebClient } from '@slack/web-api';
 
-const fetchChannelsAndMessages = async (token: string) => {
+interface UserInfo {
+  profile: {
+    image_48: string;
+  };
+  real_name: string;
+}
+
+interface Message {
+  ts: string;
+  text: string;
+  user: string;
+  userInfo?: UserInfo; // ユーザー情報がある場合にのみ定義
+  url?: string; // URLがある場合にのみ定義
+  subtype?: string; // subtypeがある場合にのみ定義
+}
+
+interface Channel {
+  id: string;
+  name: string;
+}
+
+interface ConversationsHistoryResult extends WebAPICallResult {
+  messages: Message[];
+}
+
+interface UsersInfoResult extends WebAPICallResult {
+  user: UserInfo;
+}
+
+const fetchChannelsAndMessages = async (token: string): Promise<{ channels: Channel[], messages: Message[] }> => {
   const client = new WebClient(token);
 
   try {
     // Fetch channels with "times-" prefix
     const result = await client.conversations.list({ types: 'public_channel' });
-    const timesChannels = result.channels.filter((channel: any) => {
+    const timesChannels: any[]  = result.channels.filter((channel: any) => {
       // チャンネル名が null や undefined であれば、startsWith メソッドを使用しない
       if (!channel.name) {
         return false;
@@ -24,19 +53,20 @@ const fetchChannelsAndMessages = async (token: string) => {
         const messagesResult = await client.conversations.history({
           channel: channel.id,
           limit: 5, // 50件までのメッセージを取得
-        });
+        }) as ConversationsHistoryResult;
     
         // フィルタリングとユーザー情報の取得
-        const filteredMessages = messagesResult.messages.filter((message: any) => {
+        const filteredMessages = messagesResult.messages.filter((message: Message) => {
           // 特定の条件を持つメッセージをフィルタリングする例
           return (!message.subtype || message.subtype !== 'channel_join') && (new Date(parseFloat(message.ts) * 1000) > twoWeeksAgo);// "has joined the channel" 以外のメッセージを残す  & 2週間前のメッセージを削除
 
         });
     
         // メッセージごとにユーザー情報を取得して追加
+         
         for (const message of filteredMessages) {
           if (message.user) {
-            const userInfoResult = await client.users.info({ user: message.user });
+            const userInfoResult = await client.users.info({ user: message.user }) as UsersInfoResult;
             message.userInfo = userInfoResult.user;
           }
           message.url = `https://${process.env.NEXT_PUBLIC_SLACK_WORKSPACE}.slack.com/archives/${channel.id}/p${message.ts.replace('.', '')}`;
