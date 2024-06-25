8e3627401c94e04dd28b11596e37d70e1a0edef8
鈴木安美 <ami.comsize@gmail.com>
2024-06-25 10:41:06 +0900

    ogp画像の追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,13 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
+          <meta charSet="UTF-8" />
           <link rel="icon" href="/favicon.ico" />
+          <meta property="og:title" content="Purrfect" key="ogtitle" />
+          <meta property="og:description" content="Purrfectはslackの特定チャンネルをタイムラインで表示するサービスです" key="ogdesc" />
+          <meta property="og:image" content="https://purrfect-opal.vercel.app/ogp_medium.png" key="ogimage" />
+          <meta property="og:url" content="https://purrfect-opal.vercel.app/" key="ogurl" />
+          <meta property="og:type" content="website" key="ogtype" />
         </Head>
         <body>
           <Main />
